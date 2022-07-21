ca28b7eb34aea70ae26e8bf3706b0a044602ca72
Josh Wells <josh@parall.ax>
2022-07-21 15:39:15 +0100

    Added test routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth0.authorize.optional')->get('public', fn() => [
+    'message' => 'Hello from a public endpoint! You don\'t need to be authenticated to see this.',
+    'authorized' => auth()->check(),
+    'user' => auth()->user(),
+]);
+
+Route::middleware('auth0.authorize')->get('private', fn() => [
+    'message' => 'Hello from a private endpoint! You need to be authenticated to see this.',
+    'authorized' => auth()->check(),
+    'user' => auth()->user(),
+]);
+
+Route::middleware('auth0.authorize:read:messages')->get('private-scoped', fn() => [
+    'message' => 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.',
+    'authorized' => auth()->check(),
+    'user' => auth()->user(),
+]);
